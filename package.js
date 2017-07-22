2fb8f1f22c16c9deffd45d3ebde9be8b467baea8
Ian dela Cruz <iandc76@gmail.com>
2017-07-22 15:35:24 +0800

    Moved send fence after db query

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ianpogi5:push',
-  version: '3.2.0',
+  version: '3.2.1',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
 });
